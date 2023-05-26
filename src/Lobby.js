3c34278acf6f92d85c29acb71452d28eb86542ea
202041790 Deisy Melo <deisy.melo@correounivalle.edu.co>
2023-05-26 12:26:55 -0500

    actualización: unico templo (lobby)

--- src/Lobby.js
+++ src/Lobby.js
@@ -3,16 +3,15 @@ import { useGLTF } from '@react-three/drei'
 
 export default function Lobby()
 {
-    const nodes = useGLTF("./static/lobby_temple.glb");
-    const floor= useGLTF("./static/piramidi_egitto.glb"); 
+    const nodes = useGLTF("./static/lobby_temple.glb"); 
     const map= useGLTF("./static/map_structure.glb");
-    const statue= useGLTF("./static/statue_of_ramesses_iii.glb");
+
     return (
       <group >
-        <primitive object={nodes.scene} position-y={-1.03} scale={0.005} position-x={16}/>
-        <primitive name={"arena1"} object={floor.nodes.Cube} position-x={8} position-y={-1.25} rotation-x={Math.PI * (-0.5)} />
+        <primitive object={nodes.nodes.Plane001} position-y={-1.03} scale={0.5} position-x={8} position-z={0.03}/>
+        
         <primitive name={"map structure"} object={map.scene} position-y={-0.7} scale={0.4} position-x={7.8}/>
-        <primitive name={"statue"} object={statue.scene} position-y={-0.6} scale={0.08} position-x={7.8} position-z={-2.9}/>
+
 
         
 
