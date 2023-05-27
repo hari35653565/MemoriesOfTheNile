d0aa909b2444a09017055a9bfbc001c82903f740
NicolasHuertas <98675931+NicolasHuertas@users.noreply.github.com>
2023-05-27 04:35:16 -0500

    Added movement functionality to menu

--- src/Experience.js
+++ src/Experience.js
@@ -8,18 +8,21 @@
 */
 import React from "react";
 import { OrbitControls, Text, Float } from "@react-three/drei";
-import { useThree } from "@react-three/fiber"
+import { useThree, useFrame } from "@react-three/fiber"
 import { useRef, useState, useEffect } from "react";
-import {  } from "@react-three/drei";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 import Intro from "./Intro";
 import Menu from "./Menu";
 import Skybox from "./Skybox";
-import Lobby from "./lobby";
+import Lobby from "./Lobby";
 
 export function Experience() {
 
     const [menuVisible, setMenuVisible] = useState(false);
-    const orbitControlsRef = useRef();
+    const controlsRef = useRef();
+    const { camera, gl } = useThree();
+    const cameraRef = useRef(camera);
+    const previousMouse = useRef([0, 0]);
 
     useEffect(() => {
         const handleKeyDown = (event) => {
@@ -50,11 +53,32 @@ export function Experience() {
         cameraRef.current.position.set(position[0], position[1], position[2]);
     };
 
-    const { camera } = useThree();
-    const cameraRef = useRef(camera);
+    const handleMouseMove = (event) => {
+        const { clientX, clientY } = event;
+        const [prevX, prevY] = previousMouse.current;
+        const movementX = clientX - prevX;
+        const movementY = clientY - prevY;
+    
+        if (movementX !== 0 || movementY !== 0) {
+          controlsRef.current.rotateSpeed = 1;
+          controlsRef.current.update();
+          controlsRef.current.rotateSpeed = 0.5;
+        }
+    
+        previousMouse.current = [clientX, clientY];
+      };
+    
+      useFrame(() => {
+        controlsRef.current.update();
+      });
 
     return <>
-        <OrbitControls makeDefault ref={orbitControlsRef} />
+        <OrbitControls
+            ref={controlsRef}
+            args={[camera, gl.domElement]}
+            enableRotate // Enable rotation
+            enableZoom={false} // Disable zooming
+        />
 
         <directionalLight position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { Html } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
+import Row from "react-bootstrap/Row";
 
 const Menu = ({ onClose, changeCameraPosition }) => {
     const menuRef = useRef(null);
@@ -24,16 +25,53 @@ const Menu = ({ onClose, changeCameraPosition }) => {
     return (
         <group ref={menuRef}>
         <mesh>
-            <planeBufferGeometry args={[2, 2]} />
-            <meshBasicMaterial color="transparent" />
-            <Html position={[0, 0, 0]} center>
-                {
+        <Html
+      position={[viewport.width / 2, viewport.height / 2]}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: 'Arial',
+        textAlign: 'center',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 999,
+        backgroundColor: 'orange',
+        width: '350px', // Increase the width to make it bigger
+          }}
+    >
+        <div style={{ margin:'0.5cm'}}>
+        <h2>Menú</h2>
+      <button onClick={onClose}>Cerrar</button> {/* Added a close button */}
+{
+
+                        <div className="background-image" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/static/assets/mapa.jpg)` }} >
+                            <div className="icon-row">
+                                <div className="icon" onClick={() => changeCameraPosition(0)}>
+                                    <img className="icon-image" src={`${process.env.PUBLIC_URL}/static/assets/icon.png`} />
+                                </div>
+                                <div className="icon" onClick={() => changeCameraPosition(1)}>
+                                    <img className="icon-image" src={`${process.env.PUBLIC_URL}/static/assets/icon.png`} />
+                                </div>
+                            </div>
+                            <div className="icon-row">
+                            <div className="icon" onClick={() => changeCameraPosition(2)}>
+                                    <img className="icon-image" src={`${process.env.PUBLIC_URL}/static/assets/icon.png`} />
+                                </div>
+                                <div className="icon" onClick={() => changeCameraPosition(3)}>
+                                    <img className="icon-image" src={`${process.env.PUBLIC_URL}/static/assets/icon.png`} />
+                                </div>
+                            </div>
+                            </div>
+        }
+
+    </div>
+{/*
                     <div className="menu">
                     <div className="menu-container">
-                        <div className="menu-items">
-                            <div onClick={onClose}>Cerrar Menu</div>
-                            <div onClick={onClose}>Guia</div>
-                        </div>
                         <div className="mapa-container">
                         <div className="background-image" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/static/assets/mapa.jpg)` }} >
                             <div className="icon-row">
@@ -56,7 +94,8 @@ const Menu = ({ onClose, changeCameraPosition }) => {
                         </div>
                     </div>
                     </div>
-                }
+        */}
+                
             
             </Html>
         </mesh>
